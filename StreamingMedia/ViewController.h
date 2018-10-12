6d46a59a9cf9ba5201455dae4500091a9f428577
Sl_CG00016 <allen_sun@iot-dreamcatcher.com>
2018-10-12 09:39:36 +0800

    Initial Commit

---
+++ StreamingMedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StreamingMedia
+//
+//  Created by SunLu on 2018/10/12.
+//  Copyright © 2018年 Sl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
